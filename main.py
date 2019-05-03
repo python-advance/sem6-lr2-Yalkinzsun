34c47a700e65de793492d55b2a82ea020950dcce
Kirill Skorobogatov <Kirill9806@gmail.com>
2019-05-03 13:20:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
-import numpy as np
-import threading
-from random import randint
-from datetime import datetime
-
-
-def TreadMatrix(a, b, i):
-    for j in range(len(A)):
+def run(a, b, i, n):
+    for j in range(n):
         C[i][j] = sum(map(lambda x, y: x * y, a[i], b[j]))
 
 
-n = int(input('Введите желаемую размерность матриц: '))
-A = ([[randint(-10, 10) for j in range(n)] for i in range(n)])
-B = ([[randint(-10, 10) for j in range(n)] for i in range(n)])
-B_new = np.transpose(np.array(B)).tolist()
+if __name__ == "__main__":
+    import numpy as np
+    import threading
+    from random import randint
+    from datetime import datetime
+    import multiprocessing
 
+    n = int(input('Введите желаемую размерность матриц: '))
+    A = ([[randint(-10, 10) for j in range(n)] for i in range(n)])
+    B = ([[randint(-10, 10) for j in range(n)] for i in range(n)])
+    B_new = np.transpose(np.array(B)).tolist()
+    C = ([[randint(0, 0) for j in range(n)] for i in range(n)])
+    A1 = np.array(A)
+    B1 = np.array(B)
+    print('Матрица А:\n', A1, '\n')
+    print('Матрица B:\n', B1, '\n')
 
-C = ([[randint(0, 0) for j in range(n)] for i in range(n)])
+    startTime = datetime.now()
+    for i in range(n):
+        threading.Thread(target=run, args=(A, B_new, i, n)).start()
+    endTime = datetime.now()
 
+    startTime2 = datetime.now()
+    C2 = np.dot(A1, B1)
+    endTime2 = datetime.now()
 
-A1 = np.array(A)
-B1 = np.array(B)
+    print('A*B (потоки) \n', np.array(C))
+    print(f'Затраченное время: {endTime - startTime} \n')
 
+    print('A*B (numpy) \n', C2)
+    print(f'Затраченное время: {endTime2 - startTime2} \n')
 
-startTime = datetime.now()
+    print(f'Рекомендуемое число потоков = {multiprocessing.cpu_count()}')
 
-for i in range(len(A)):
-    threading.Thread(target=TreadMatrix, args=(A, B_new, i)).start()
-
-endTime = datetime.now()
-
-startTime2 = datetime.now()
-C2 = np.dot(A1, B1)
-
-endTime2 = datetime.now()
-
-print('Сгенерированная матрица: \n', C2)
-print('Затраченное время (threads):', endTime-startTime)
-print('Затраченное время (numpy): ', endTime2-startTime2)
